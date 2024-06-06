ab9da202efdd11e9545aba098307ff926687b16d
eunxoo <dmstn7989@naver.com>
2024-06-07 08:24:56 +0900

    init: 디렉터리 구조 세팅

--- src/pages/layout/MainLayout.jsx
+++ src/pages/layout/MainLayout.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default MainLayout;
