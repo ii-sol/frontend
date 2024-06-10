8052ac600df4ec321bbd325fe6d6609e801ff77e
eunxoo <dmstn7989@naver.com>
2024-06-10 16:56:36 +0900

    feat: 투자 header

--- src/pages/layout/InsideLayout.jsx
+++ src/pages/layout/InsideLayout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import tw from "twin.macro";
+import { MdArrowBackIos } from "react-icons/md";
 
-const InsideLayout = () => {
+const InsideLayout = ({ service }) => {
   return (
     <div tw="flex flex-col h-screen">
-      <header tw="mt-5 flex justify-between items-center p-6">
-        <button tw="text-xl">{"<"}</button>
-        <h1 tw="text-2xl font-bold">대출</h1>
-        <button tw="text-xl">취소</button>
+      <header tw="flex justify-between items-center p-4">
+        <button tw="text-2xl">
+          <MdArrowBackIos />
+        </button>
+        <h1 tw="text-2xl font-bold">{service}</h1>
+        <button tw="text-lg">취소</button>
       </header>
       <main tw="flex-1">
         <Outlet />
