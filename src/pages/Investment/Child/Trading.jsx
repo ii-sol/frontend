8a65a2990ca6868ab5ba93003f00b6986ba0ce19
eunxoo <dmstn7989@naver.com>
2024-06-10 17:03:39 +0900

    feat: 매도매수 페이지

---
+++ src/pages/Investment/Child/Trading.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import StocksAbout from "../../../components/Investment/StocksAbout";
+import Header from "../../../components/Investment/Header";
+import Keypad from "../../../components/common/Keypad";
+import { styled } from "styled-components";
+import * as S from "../../../styles/GlobalStyles";
+import { useLocation } from "react-router-dom";
+
+//TODO: 매수주문증거금이 부족합니다. alert
+const Trading = ({ account }) => {
+  const { state } = useLocation();
+  console.log(state);
+  const [displayedNumber, setDisplayedNumber] = useState("0");
+  const handleNumberClick = (number) => {
+    if (displayedNumber.length < 7) {
+      setDisplayedNumber((prevNumber) => prevNumber + number);
+    }
+  };
+
+  const handleBackspace = () => {
+    if (displayedNumber.length > 1) {
+      setDisplayedNumber((prevNumber) => prevNumber.slice(0, -1));
+    }
+  };
+
+  const normalizeNumber = (number) => {
+    return parseFloat(number).toLocaleString("en-US");
+  };
+
+  // TODO: 계좌 확인, 매수매도 확인
+  const onNav = (type) => {};
+  return (
+    <div>
+      <Header type="none" />
+      <StocksAbout />
+      <ColumDiv>
+        {state.type === "buy" ? (
+          <InfoDiv>얼마나 살까요?</InfoDiv>
+        ) : (
+          <InfoDiv>얼마나 팔까요?</InfoDiv>
+        )}
+        <Amount>
+          {normalizeNumber(displayedNumber)} <span>주</span>
+        </Amount>
+        <Div>주문금액 52,425원</Div>
+        <Keypad
+          onNumberClick={handleNumberClick}
+          onBackspace={handleBackspace}
+        />
+        {state.type === "buy" ? (
+          <S.BuyBtn $background="#FF5959" onClick={() => onNav("buy")}>
+            구매하기
+          </S.BuyBtn>
+        ) : (
+          <S.BuyBtn $background="#5987ff">판매하기</S.BuyBtn>
+        )}
+      </ColumDiv>
+    </div>
+  );
+};
+
+export default Trading;
+
+const ColumDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const InfoDiv = styled.div`
+  color: #000000;
+  font-size: 22px;
+  text-align: center;
+  margin: 10px auto;
+`;
+
+const InputDiv = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  background-color: #f5f5f5;
+  width: 120px;
+  height: 40px;
+  border-radius: 100px;
+  padding-right: 40px;
+
+  font-size: 20px;
+`;
+
+const Amount = styled.div`
+  position: relative;
+  width: 105px;
+  height: 49px;
+  background: #f5f5f5;
+  padding: 10px;
+  border-radius: 15px;
+  font-size: 18px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  text-align: right;
+`;
+
+const Div = styled.div`
+  font-size: 20px;
+  margin-left: auto;
+  margin-bottom: 10px;
+  color: #707070;
+  font-size: 18px;
+`;
