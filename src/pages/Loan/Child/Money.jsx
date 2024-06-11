73dc4708021cdaa52de64bd59d412d025c3bc311
NOEL-code <junusong12@naver.com>
2024-06-11 11:25:46 +0900

    feat: 대출 페이지 기능추가

--- src/pages/Loan/Child/Money.jsx
+++ src/pages/Loan/Child/Money.jsx
@@ -50,6 +50,9 @@ const Money = () => {
         <div tw="mt-4">
           <NextButton onClick={handleNext} />
         </div>
+        <div tw="mt-8">
+          <NextButton onClick={handleNext} />
+        </div>
       </main>
     </div>
   );
