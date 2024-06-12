d6231936d5d207a9dc34e00ac3bd9b0ffde64a50
NOEL-code <junusong12@naver.com>
2024-06-12 13:05:58 +0900

    feat: 부모 아이 페이지 구분

--- src/pages/Loan/Child/Message.jsx
+++ src/pages/Loan/Child/Message.jsx
@@ -6,17 +6,23 @@ import NextButton from "../../../components/Loan/NextButton";
 
 const Message = () => {
   const [messageText, setMessageText] = useState("");
+  const [titleText, setTitleText] = useState("");
   const [error, setError] = useState("");
-  const textareaRef = useRef(null);
+  const messageRef = useRef(null);
+  const titleRef = useRef(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     // Adjust the height of the textarea to fit the content
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "auto";
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    if (messageRef.current) {
+      messageRef.current.style.height = "auto";
+      messageRef.current.style.height = `${messageRef.current.scrollHeight}px`;
     }
-  }, [messageText]);
+    if (titleRef.current) {
+      titleRef.current.style.height = "auto";
+      titleRef.current.style.height = `${titleRef.current.scrollHeight}px`;
+    }
+  }, [messageText, titleText]);
 
   const handleChange = (e) => {
     if (e.target.value.length <= 50) {
@@ -25,11 +31,18 @@ const Message = () => {
     }
   };
 
+  const handleTitleChange = (e) => {
+    if (e.target.value.length <= 10) {
+      setTitleText(e.target.value);
+      setError("");
+    }
+  };
+
   const handleNext = () => {
-    if (messageText.length > 0) {
+    if (messageText.length > 0 && titleText.length > 0) {
       navigate("/loan/complete");
     } else {
-      setError("메세지를 1자 이상 입력해주세요.");
+      setError("제목과 메세지를 1자 이상 입력해주세요.");
     }
   };
 
@@ -37,23 +50,34 @@ const Message = () => {
     <div tw="flex flex-col h-screen justify-between">
       <div tw="flex flex-col items-center p-5">
         <img src={message} tw="mb-4" alt="Message" />
-        <div tw="flex justify-center items-center bg-blue-100 p-6 rounded-2xl w-full">
-          <div tw="flex justify-center items-center bg-white px-4 py-4 rounded-2xl w-full">
-            <textarea
-              ref={textareaRef}
-              value={messageText}
-              onChange={handleChange}
-              rows={1} // Initial rows to keep it small
-              tw="resize-none w-full outline-none border-none text-center text-lg overflow-hidden"
-              placeholder="메세지를 입력하세요..."
-            />
+        <div tw="w-full max-w-md">
+          <div tw="bg-blue-100 p-6 rounded-2xl mb-4">
+            <div tw="bg-white p-3 rounded-2xl mb-4">
+              <textarea
+                ref={titleRef}
+                value={titleText}
+                onChange={handleTitleChange}
+                rows={1}
+                tw="resize-none w-full outline-none border-none text-center text-lg overflow-hidden"
+                placeholder="제목을 입력하세요."
+              />
+            </div>
+            <div tw="bg-white p-3 rounded-2xl">
+              <textarea
+                ref={messageRef}
+                value={messageText}
+                onChange={handleChange}
+                rows={1}
+                tw="resize-none w-full outline-none border-none text-center text-lg overflow-hidden"
+                placeholder="메세지를 입력하세요"
+              />
+            </div>
           </div>
+          <p tw="text-gray-500 text-center">{messageText.length}/50</p>
+          {error && <p tw="text-red-500 text-center mt-2">{error}</p>}
         </div>
-        <p tw="text-gray-500 mt-2">{messageText.length}/50</p>
-        {error && <p tw="text-red-500 mt-2">{error}</p>}
       </div>
-
-      <footer tw="fixed bottom-2 left-0 right-0 w-full p-4">
+      <footer tw="p-4">
         <NextButton onClick={handleNext} />
       </footer>
     </div>
