0177e8e677ba6f616756b831012c5a79a7b31de8
NOEL-code <junusong12@naver.com>
2024-06-19 14:05:21 +0900

    Update HistoryList.jsx

--- src/components/common/History/HistoryList.jsx
+++ src/components/common/History/HistoryList.jsx
@@ -7,6 +7,10 @@ import { useLocation } from "react-router-dom";
 import SuggestHistory from "../../Investment/SuggestHistoryList";
 import TradeHistory from "../../Investment/TradeHistoryList";
 import ChildAllowanceHistory from "../../Allowance/ChildHistoryListItem";
+<<<<<<< Updated upstream
+=======
+//import ParentAllowanceHistory from "../../Allowance/ParentHistoryListItem";
+>>>>>>> Stashed changes
 import IrregularAllowanceHistory from "../../Allowance/IrregularHistoryListItem";
 import MissionHistory from "../../Mission/MissionHistoryListItem";
 import LoanHistoryListItem from "../../../pages/Loan/Child/LoanHistoryListItem";
@@ -23,7 +27,13 @@ const HistoryList = () => {
           <TradeHistory />
         ) : location.pathname === "/allowance/history" ? (
           <ChildAllowanceHistory />
+<<<<<<< Updated upstream
         ) : location.pathname === "/allowance/irregular/history" ? (
+=======
+        ) : // ) : location.pathname === "/allowance/history-parent" ? (
+        //   <ParentAllowanceHistory />
+        location.pathname === "/allowance/irregular/history" ? (
+>>>>>>> Stashed changes
           <IrregularAllowanceHistory />
         ) : location.pathname === "/mission/history" ? (
           <MissionHistory />
