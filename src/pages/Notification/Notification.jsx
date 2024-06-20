6bc2b20be78503de69294132140702f92fe8e1ca
eunxoo <dmstn7989@naver.com>
2024-06-20 14:54:26 +0900

    feat: 알림 전체 삭제 alert

--- src/pages/Notification/Notification.jsx
+++ src/pages/Notification/Notification.jsx
@@ -5,23 +5,34 @@ import { useNavigate } from "react-router-dom";
 import { MdArrowBackIos } from "react-icons/md";
 import Filter from "../../components/Notification/Filter";
 import NotiList from "../../components/Notification/NotiList";
+import Delete from "../../assets/img/Invest/delete.svg";
+import CheckDeleteNoti from "../../components/common/Alert/CheckDeleteNoti";
+import { BottomSheet } from "react-spring-bottom-sheet";
 
 const Notification = () => {
   const navigate = useNavigate();
   const handleLeftClick = () => {
     navigate("/");
   };
+  const [open, setOpen] = useState(false);
 
+  const handleDismiss = () => {
+    setOpen(false);
+  };
   return (
     <S.Container>
       <Header
         left={<MdArrowBackIos />}
         onLeftClick={handleLeftClick}
         title={"알림"}
-        right={""}
+        right={<img src={Delete} alt="Delete" width={25} />}
+        onRightClick={() => setOpen(true)}
       />
       <Filter />
       <NotiList />
+      <BottomSheet open={open} onDismiss={handleDismiss}>
+        <CheckDeleteNoti onDismiss={handleDismiss} />
+      </BottomSheet>
     </S.Container>
   );
 };
