7cf32625ca98c8236079cf23c2f908ca0ef078f0
eunxoo <dmstn7989@naver.com>
2024-06-23 12:21:40 +0900

    feat: 홈 로그인 안 한 화면 구현

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import isLogin from "./isLogin";
 
 const PrivateRoute = ({ children }) => {
-  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+  const isLoggedIn = isLogin();
 
   return isLoggedIn ? children : <Navigate to="/login" />;
 };
